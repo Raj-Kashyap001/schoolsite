5ffe2d4aaf09ffe903b5687f9d0f10fa86b4c271
Raj Kashyap <rajkashyapatservice@outlook.com>
2025-09-30 11:42:29 +0530

    basic styles done!

---
+++ hbr/static/js/main.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+openMobileNav.addEventListener("click", () => {
+  mobileNav.classList.add("active");
+});
+
+closeMobileNav.addEventListener("click", () => {
+  mobileNav.classList.remove("active");
+});
