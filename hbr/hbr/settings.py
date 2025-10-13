f8b3648bac2b8cd4fc41b75f02cae27a9ebfeed2
Raj Kashyap <138576518+Raj-Kashyap001@users.noreply.github.com>
2025-10-13 08:31:48 +0530

    Updated settings.py

--- hbr/hbr/settings.py
+++ hbr/hbr/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -132,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'  # ← must start and end with /
 STATICFILES_DIRS = [BASE_DIR / 'static']  # your dev/static folder
 STATIC_ROOT = BASE_DIR / 'staticfiles'    # where collectstatic puts files
